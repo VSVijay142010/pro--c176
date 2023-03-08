6b345c2528891508d0983a5969fb00587b62840d
VS VIJAY /வா.ச.விஜய்/वा.स.विजय/వా.సా.విజయ్ <85885759+VSVijay142010@users.noreply.github.com>
2023-03-08 21:27:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+document.write("<div class=\"row text-center\"><div class=\"col-sm-4 text-center\"><h1>GUESS ME!</h1><p>Click on the alphabets to guess the word!</p></div></div>");
+document.write(" <div class=\"row text-center\">");
+document.write("            <div class=\"col-sm-4 text-center\" id=\"alphabets\">");
+document.write("                <button type=\"button\" id=\"a\" class=\"btn btn-outline-primary clickable\">a</button>");
+document.write("                <button type=\"button\" id=\"b\" class=\"btn btn-outline-primary clickable\">b</button>");
+document.write("                <button type=\"button\" id=\"c\" class=\"btn btn-outline-primary clickable\">c</button>");
+document.write("                <button type=\"button\" id=\"d\" class=\"btn btn-outline-primary clickable\">d</button>");
+document.write("                <button type=\"button\" id=\"e\" class=\"btn btn-outline-primary clickable\">e</button>");
+document.write("                <button type=\"button\" id=\"f\" class=\"btn btn-outline-primary clickable\">f</button>");
+document.write("                <button type=\"button\" id=\"g\" class=\"btn btn-outline-primary clickable\">g</button>");
+document.write("                <button type=\"button\" id=\"h\" class=\"btn btn-outline-primary clickable\">h</button>");
+document.write("                <button type=\"button\" id=\"i\" class=\"btn btn-outline-primary clickable\">i</button>");
+document.write("                <button type=\"button\" id=\"j\" class=\"btn btn-outline-primary clickable\">j</button>");
+document.write("                <button type=\"button\" id=\"k\" class=\"btn btn-outline-primary clickable\">k</button>");
+document.write("                <button type=\"button\" id=\"l\" class=\"btn btn-outline-primary clickable\">l</button>");
+document.write("                <button type=\"button\" id=\"m\" class=\"btn btn-outline-primary clickable\">m</button>");
+document.write("                <button type=\"button\" id=\"n\" class=\"btn btn-outline-primary clickable\">n</button>");
+document.write("                <button type=\"button\" id=\"o\" class=\"btn btn-outline-primary clickable\">o</button>");
+document.write("                <button type=\"button\" id=\"p\" class=\"btn btn-outline-primary clickable\">p</button>");
+document.write("                <button type=\"button\" id=\"q\" class=\"btn btn-outline-primary clickable\">q</button>");
+document.write("                <button type=\"button\" id=\"r\" class=\"btn btn-outline-primary clickable\">r</button>");
+document.write("                <button type=\"button\" id=\"s\" class=\"btn btn-outline-primary clickable\">s</button>");
+document.write("                <button type=\"button\" id=\"t\" class=\"btn btn-outline-primary clickable\">t</button>");
+document.write("                <button type=\"button\" id=\"u\" class=\"btn btn-outline-primary clickable\">u</button>");
+document.write("                <button type=\"button\" id=\"v\" class=\"btn btn-outline-primary clickable\">v</button>");
+document.write("                <button type=\"button\" id=\"w\" class=\"btn btn-outline-primary clickable\">w</button>");
+document.write("                <button type=\"button\" id=\"x\" class=\"btn btn-outline-primary clickable\">x</button>");
+document.write("                <button type=\"button\" id=\"y\" class=\"btn btn-outline-primary clickable\">y</button>");
+document.write("                <button type=\"button\" id=\"z\" class=\"btn btn-outline-primary clickable\">z</button>");
+document.write("            </div>");
+document.write("        </div>");
+document.write("        </div>");
+document.write("");
+document.write("        <div class=\"row text-center\">");
+document.write("            <div class=\"col-sm-4 text-center\">");
+document.write("                <span>Hint: Category is</span>");
+document.write("                <span id=\"hint\"></span>");
+document.write("            </div>");
+document.write("        </div>");
+document.write("");
+document.write("        <div class=\"row text-center\">");
+document.write("            <div class=\"col-sm-4 text-center\">");
+document.write("                <p id=\"blanks\"></p>");
+document.write("            </div>");
+document.write("        </div>");
+document.write("    </div>");
+document.write("");
+document.write("    <div class=\"row text-center\">");
+document.write("        <div class=\"col-sm-4 text-center\">");
+document.write("            <p id=\"result\"></p>");
+document.write("        </div>");
+
+
+
+
